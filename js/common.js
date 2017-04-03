27a7d50f74cf46e24bc62a2e2c8f65e7429b81b8
liuyangyangwork <liuyangyangwork@163.com>
2017-04-03 14:18:27 +0800

    showpage

---
+++ js/common.js
@@ -0,0 +1,13 @@
+// /*
+// *onTextMenu
+// */
+//  document.oncontextmenu = function(ev){
+//    var ev=ev||event
+//     $("#text-menu").css("display","block")
+//     $("#text-menu").css("top",ev.clientY)
+//     $("#text-menu").css("left",ev.clientX)
+//     return false;
+//   }
+//   document.onclick=function(){
+//      $("#text-menu").css("display","none")
+//   }
