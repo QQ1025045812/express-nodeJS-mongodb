63e8d62a8c92db02bc2c8b7e776c13d085cfdf54
QQ1025045812 <1025045812@qq.com>
2017-04-10 17:47:07 +0800

    change

--- app.js
+++ app.js
@@ -4,39 +4,39 @@
  * 
 */
 //加载express模块
-var express=require('express');
+var express = require('express');
 //加载模板
-var swig=require('swig');
-var mongoose=require('mongoose');
+var swig = require('swig');
+var mongoose = require('mongoose');
 //加载bodyParser
-var bodyParser=require('body-parser');
+var bodyParser = require('body-parser');
 //加cookie模块
-var Cookies=require('cookies');
+var Cookies = require('cookies');
 //创建app应用
-var app=express();
+var app = express();
 //设置静态文件托管
-app.use('/public',express.static(__dirname+'/public'));
+app.use('/public', express.static(__dirname + '/public'));
 //配置模板应用
 //第一个参数，定义当前模板所使用的模板引擎，模板引擎的名称，同时也是模板文件的后缀,第二个函数表示解析模板内容的方法
-app.engine('html',swig.renderFile);
+app.engine('html', swig.renderFile);
 //设置模板文件的存放目录,第一个参数必须是views，第二个是路径
-app.set('views','./views');
+app.set('views', './views');
 //注册所使用的模板引擎，第一个参数必须是view engine；第二个参数和app.engine这个方法中定义的第一个函数也就是模板引擎名字一致
-app.set('view engine','html');
+app.set('view engine', 'html');
 
 //取消模板缓存
-swig.setDefaults({cache:false});
+swig.setDefaults({ cache: false });
 //bodyParser设置
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //设置cookies
-app.use(function(req,res,next){
-    req.cookies=new Cookies(req,res);
-    req.userInfo={};
-    if(req.cookies.get('userInfo')){
-        try{
-        req.userInfo=JSON.parse(req.cookies.get('userInfo'))
-        }catch(e){
-            
+app.use(function (req, res, next) {
+    req.cookies = new Cookies(req, res);
+    req.userInfo = {};
+    if (req.cookies.get('userInfo')) {
+        try {
+            req.userInfo = JSON.parse(req.cookies.get('userInfo'))
+        } catch (e) {
+
         }
     }
     next();
@@ -54,12 +54,12 @@ app.use(function(req,res,next){
 //     res.render('index');
 // })
 //根据不同的功能划分模块
-app.use('/admin',require('./routers/admin'));
-app.use('/api',require('./routers/api'));
-app.use('/',require('./routers/main'));
-app.use('/houtai',require('./routers/houtai'));
-mongoose.connect('mongodb://localhost:27018/guanwang',function(err){
-    if(err){
+app.use('/admin', require('./routers/admin'));
+app.use('/api', require('./routers/api'));
+app.use('/', require('./routers/main'));
+app.use('/houtai', require('./routers/houtai'));
+mongoose.connect('mongodb://localhost:27018/guanwang', function (err) {
+    if (err) {
         console.log("数据库连接失败！！！")
         return;
     }
--- routers/houtai.js
+++ routers/houtai.js
@@ -1,80 +1,31 @@
-var express=require('express');
-var router=express.Router();
-var Banner=require('../models/banner');
-var Roms=require('../models/roms');
-var Chanpin=require('../models/chanpin');
-var User=require('../models/user');
+var express = require('express');
+var router = express.Router();
+var Banner = require('../models/banner');
+var Roms = require('../models/roms');
+var Chanpin = require('../models/chanpin');
 var responseData;
-router.use(function(req,res,next){
-    responseData={
-        code:0,
-        message:''
-    };
-    next();
-})
-router.post('/login',function(req,res,next){
-    var username=req.body.username;
-    var password=req.body.password;
-    // // var user=new User({
-    // //     username:username,
-    // //     password:password
-    // // });
-    // // console.log(user)
-    // return user.save();
-    if(username==''||password==''){
-        responseData.code=1;
-        responseData.message='用户名或密码不能为空';
-        return res.json(responseData);
+router.use(function (req, res, next) {
+  if (!req.cookies.get('userInfo')) {
+    try {
+      return res.send('对不起，只有管理员才能访问网站后台')
+    } catch (e) {
     }
-    User.findOne({
-        username:username,
-        password:password
-    }).then(function(userInfo){
-        if(!userInfo){
-            responseData.code=2;
-            responseData.message='用户名或密码错误';
-            return res.json(responseData);
-        }else{
-            responseData.message='登陆成功';
-            responseData.userInfo={
-                _id:userInfo.id,
-                username:userInfo.username
-            };
-            req.cookies.set('userInfo',JSON.stringify({
-                _id:userInfo.id,
-                username:userInfo.username
-            }));
-            res.json(responseData);
-        }
-    })
-});
-router.use(function(req,res,next){
-  if(!req.cookies.get('userInfo')){
-        try{
-          return res.send('对不起，只有管理员才能访问网站后台')
-        }catch(e){
-
-        }
-    };
- next();
-});
-router.get('/',function(req,res,next){
+  };
+  responseData = {
+    message: ''
+  };
+  next();
+})
+router.get('/', function (req, res, next) {
   res.render('houtai');
 })
-router.get('/bannerhoutai',function(req,res,next){
-  // var title="001";
-  // var url="/public/images/banner4.jpg"
-  // var banner=new Banner({
-  //       title:title,
-  //       url:url
-  //   });
-  // banner.save();
-  Banner.find().then(function(banners){
-    res.render('bannerhoutai',{banners:banners});
+router.get('/bannerhoutai', function (req, res, next) {
+  Banner.find().then(function (banners) {
+    res.render('bannerhoutai', { banners: banners });
   })
 })
-router.get('/chanpinhoutai',function(req,res,next){
-   // var title="001";
+router.get('/chanpinhoutai', function (req, res, next) {
+  // var title="001";
   // var url="/public/images/banner4.jpg"
   // var banner=new Banner({
   //       title:title,
@@ -102,46 +53,61 @@ router.get('/chanpinhoutai',function(req,res,next){
   //   desc:"String",
   //   environment: [],
   //   url:"String",
-    
+
   // });
   //  chanpin.save();
-  Chanpin.find().then(function(chanpin){
-    res.render('chanpinhoutai',{chanpin:chanpin});
+  Chanpin.find().then(function (chanpin) {
+    res.render('chanpinhoutai', { chanpin: chanpin });
   })
 })
-router.get('/romshoutai',function(req,res,next){
-  //   var title="智能辐射监测及OA管理系统软件";
-  // var url="/public/images/pro03.png";
-  // var subtitle="ROMS100/ROMS200系列";
-  //   var desc=["利用物联网和“互联网+”环境监管的理念，设计的一整套“监测任务派发”、“现场智能监测”、“质控多级审核”、“原始记录一键生成报告”、“大数据管理”等功能为一体的智能辐射监测OA软件系统。","包括私有云服务器端、WEB端、APP监测和短信电话推送后台"];
-  // var roms=new Roms({
-  //       title:title,
-  //       url:url,
-  //       subtitle:subtitle,
-  //       desc:desc
-  //   });
-  // roms.save();
-  Roms.find().then(function(roms){
-    res.render('romshoutai',{roms:roms});
+router.get('/romshoutai', function (req, res, next) {
+  Roms.find().then(function (roms) {
+    res.render('romshoutai', { roms: roms });
   })
 })
-router.post('/loginout',function(req,res,next){
-  req.cookies.set('userInfo',null);
-  console.log(111)
-  console.log(req.cookies.get('userInfo'))
-  console.log(111)
-   res.send("sucess")
+router.post('/loginout', function (req, res, next) {
+  req.cookies.set('userInfo', null);
+  res.send("sucess")
 })
-router.get('/jiancehoutai',function(req,res,next){
+router.get('/jiancehoutai', function (req, res, next) {
   res.render('jiancehoutai');
 })
-router.get('/yanfahoutai',function(req,res,next){
+router.get('/yanfahoutai', function (req, res, next) {
   res.render('yanfahoutai');
 })
-router.get('/abouthoutai',function(req,res,next){
+router.get('/abouthoutai', function (req, res, next) {
   res.render('abouthoutai');
 })
-router.get('/banneredit',function(req,res,next){
-  res.render('banneredit');
+router.get('/banneredit', function (req, res, next) {
+  var id = req.query.id || '';
+  Banner.findOne({
+    _id: id
+  }).then(function (bannerInfo) {
+    if (!bannerInfo) {
+      return res.send('banner信息不存在');
+    } else {
+      res.render('banneredit', {
+        bannerInfo: bannerInfo
+      });
+    };
+  })
+});
+router.post('/banneredit', function (req, res, next) {
+  var title = req.body.title || '';
+  var url = req.body.url || '';
+  var id = req.query.id || '';
+  Banner.findOne({
+    _id: '58e5e51d36a06159e59835a4'
+  }).then(function (bannerInfo) {
+    if (!bannerInfo) {
+      return res.send('banner信息不存在');
+    } else {
+      if (title == bannerInfo.title && url == bannerInfo.url) {
+        return res.send('保存成功');
+      }else{
+        return res.send('保存成功2');
+      }
+    };
+  })
 })
-module.exports=router;
+module.exports = router;
